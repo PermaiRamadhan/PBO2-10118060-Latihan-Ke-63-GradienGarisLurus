20d7cf5e6e6c5740274ce96384b69660b878e6dc
Synxsaints <Synxsaints@LAPTOP-EAMMNJ87>
2019-10-24 22:22:00 +0700

    First Commit

---
+++ src/pbo2/pkg10118060/latihan/ke/pkg63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118060.latihan.ke.pkg63.gradiengarislurus;
+
+/**
+ *
+ * @author 
+ * NAMA     : Permai Ramadhan
+ * KELAS    : IF-2
+ * NIM      : 10118060
+ * Deskripsi Program : Membuat program dengan konsep abstraksi untuk menghitung
+ * gradien
+ */
+public interface GarisLurus {
+    public int hitungGradien();
+    
+}
